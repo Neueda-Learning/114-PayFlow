90123e655693f5406153fdd040b1b9e3cb42c6f6
Dhruv Sharma <sharmasdhruv007@gmail.com>
2026-08-04 06:22:10 +0000

    Remove leftover old com.flowpay package after PayFlow rename merge

--- backend/src/main/java/com/flowpay/repository/ReceivingAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.flowpay.repository;
-
-import com.flowpay.model.ReceivingAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReceivingAccountRepository extends JpaRepository<ReceivingAccount, Long> {
-}
